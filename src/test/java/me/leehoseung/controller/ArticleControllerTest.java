1fdcbedebd0892a2b3ce53c3da42f2ec9815d0ca
MeteorLee <meteorproject529@gmail.com>
2024-10-01 23:45:48 +0900

    test: ArticleControllerTest 생성, 게시글 추가 기능 테스트
    Controller 테스트를 위한 설정 완료
    게시글 추가 기능 테스트 추가

---
+++ src/test/java/me/leehoseung/controller/ArticleControllerTest.java
@@ -0,0 +1,74 @@
+package me.leehoseung.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.leehoseung.domain.Article;
+import me.leehoseung.dto.AddArticleRequest;
+import me.leehoseung.repository.ArticleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ArticleControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @BeforeEach
+    public void mockMvcSetUP() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        articleRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 블로그 글 추가에 성공한다.")
+    @Test
+    void addArticle() throws Exception {
+        // Given
+        final String url = "/api/articles";
+        final String title = "제목";
+        final String content = "내용";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        // When
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        // Then
+        result
+                .andExpect(status().isCreated());
+
+        List<Article> articles = articleRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+
+    }
+}

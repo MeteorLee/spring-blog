9fdbf2cd2a592407ac9b23d2dc1184ef4abf35d8
MeteorLee <meteorproject529@gmail.com>
2024-10-02 15:36:03 +0900

    chore: 게시글 글 목록 조회 DTO 생성
    게시글 글 목록 조회 ArticleResponse.java DTO 생성

---
+++ src/main/java/me/leehoseung/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package me.leehoseung.dto;
+
+import lombok.Getter;
+import me.leehoseung.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}

971269f8fa3b9d4ade3ed4d823b8f726b99faad3
MeteorLee <meteorproject529@gmail.com>
2024-10-02 15:35:18 +0900

    feat: 블로그 글 목록 조회 API 구현
    블로그 글 목록 조회를 구현

--- src/main/java/me/leehoseung/controller/ArticleController.java
+++ src/main/java/me/leehoseung/controller/ArticleController.java
@@ -3,12 +3,16 @@ package me.leehoseung.controller;
 import lombok.RequiredArgsConstructor;
 import me.leehoseung.domain.Article;
 import me.leehoseung.dto.AddArticleRequest;
+import me.leehoseung.dto.ArticleResponse;
 import me.leehoseung.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,4 +26,16 @@ public class ArticleController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = articleService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
 }
--- src/main/java/me/leehoseung/service/ArticleService.java
+++ src/main/java/me/leehoseung/service/ArticleService.java
@@ -6,6 +6,8 @@ import me.leehoseung.dto.AddArticleRequest;
 import me.leehoseung.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
@@ -14,5 +16,9 @@ public class ArticleService {
 
     public Article save(AddArticleRequest request) {
         return articleRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return articleRepository.findAll();
     }
 }

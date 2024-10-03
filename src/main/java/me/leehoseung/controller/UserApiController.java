b49ba1c82095994668b78b6dcf45b4dc0e850731
MeteorLee <meteorproject529@gmail.com>
2024-10-03 21:27:58 +0900

    feat: 회원 로그아웃 구현
    로그 아웃 기능을 구현함.

--- src/main/java/me/leehoseung/controller/UserApiController.java
+++ src/main/java/me/leehoseung/controller/UserApiController.java
@@ -1,9 +1,14 @@
 package me.leehoseung.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.leehoseung.dto.AddUserRequest;
 import me.leehoseung.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -20,4 +25,10 @@ public class UserApiController {
 
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:/login";
+    }
 }

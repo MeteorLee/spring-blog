bd8ccc1bfebde15e9c663afd16c1ad41683911da
MeteorLee <meteorproject529@gmail.com>
2024-10-03 16:36:33 +0900

    chorea: springJpaAuditing 사용 가능 설정
    엔티티 Auditing 기능을 사용하기 위한 설정을 추가함

--- src/main/java/me/leehoseung/SpringBlogApplication.java
+++ src/main/java/me/leehoseung/SpringBlogApplication.java
@@ -2,7 +2,9 @@ package me.leehoseung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBlogApplication {
     public static void main(String[] args) {

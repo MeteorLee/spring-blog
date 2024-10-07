6e37bb3db2a1ddbaffd56626bd7a2166fb96b7cd
MeteorLee <meteorproject529@gmail.com>
2024-10-07 20:06:45 +0900

    chore: 리프레시 토큰 검색 구현
    리프레시 토큰 토큰 스트링값으로 검색하는 기능을 구현
    예외 발생 시 IllegalArgumentException 발생

---
+++ src/main/java/me/leehoseung/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package me.leehoseung.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leehoseung.domain.RefreshToken;
+import me.leehoseung.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}

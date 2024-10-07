0070ff2a997d3900d50f9262d4129ed8fe38b76c
MeteorLee <meteorproject529@gmail.com>
2024-10-08 01:18:07 +0900

    feat: oauth2 구현
    추후 다시 공부 필요

--- src/main/java/me/leehoseung/service/UserService.java
+++ src/main/java/me/leehoseung/service/UserService.java
@@ -1,29 +1,35 @@
-    package me.leehoseung.service;
+package me.leehoseung.service;
 
-    import lombok.RequiredArgsConstructor;
-    import me.leehoseung.domain.User;
-    import me.leehoseung.dto.AddUserRequest;
-    import me.leehoseung.repository.UserRepository;
-    import org.springframework.security.crypto.password.PasswordEncoder;
-    import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import me.leehoseung.domain.User;
+import me.leehoseung.dto.AddUserRequest;
+import me.leehoseung.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
-    @RequiredArgsConstructor
-    @Service
-    public class UserService {
+@RequiredArgsConstructor
+@Service
+public class UserService {
 
-        private final UserRepository userRepository;
-        private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+//        private final PasswordEncoder passwordEncoder;
 
-        public Long save(AddUserRequest dto) {
-            return userRepository.save(User.builder()
-                    .email(dto.getEmail())
-                    .password(passwordEncoder.encode(dto.getPassword()))
-                    .build()).getId();
-        }
+    public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-        public User findById(long userId) {
-            return userRepository.findById(userId)
-                    .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
-        }
-
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build()).getId();
     }
+
+    public User findById(long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+}
